cd8cc17b7a73500f883a3f23f115f796987249ea
Praise <prakty1205@gmail.com>
2025-03-11 15:44:01 -0600

    updated import statement for prisma client in root file

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import router from "./routes/index.js"
+import prisma from "./utils/dbConfig.js"
 
 const app = express()
 dotenv.config()
