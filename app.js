61314f5b787b7257958a1cf4d53502ceb010c9b1
Praise <prakty1205@gmail.com>
2025-03-11 15:41:55 -0600

    added a connect to db function

--- app.js
+++ app.js
@@ -13,8 +13,19 @@ app.use('/api/v1', router)
 
 const port = process.env.PORT || 5000
 
+async function connectToDB() {
+    try {
+        await prisma.$disconnect()
+    } catch (error) {
+        console.error(error.message)
+        await prisma.$disconnect()
+        process.exit(1)
+    }
+}
 
 
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`)
 })
+
+connectToDB()
